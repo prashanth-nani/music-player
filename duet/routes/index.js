36087f55264bf877bd5158e99eeb196b12832f87
Prashanth <prashanth1395@gmail.com>
2016-07-18 23:59:13 +0530

    Remove server side code and use audio tags in client side

--- duet/routes/index.js
+++ duet/routes/index.js
@@ -1,25 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-
-var MPlayer = require('mplayer');
-
-options = {
-	debug: false
-};
-
-console.log('I\'m here');
-var player = new MPlayer(options);
-
-player.on('start', console.log.bind(this, 'playback started'));
-player.on('status', console.log);
-
-player.setOptions({
-	volume: 100
-});
-player.openFile('./1.mp3');
-
-setTimeout(player.volume.bind(player, 100), 1000);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
